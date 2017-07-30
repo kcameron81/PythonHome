b3a9e3e2144784d15e941c36185dc0ea9e3e5e31
kcameron81 <kevcameron@gmail.com>
2017-07-30 23:55:23 +0100

    Initial Test of pushign back to GitHub

---
+++ BasicMath.py
@@ -0,0 +1,6 @@
+import math
+
+xString = input('Give me a number: ')
+numeric_input = int(xString)
+sqrt_output = math.sqrt(numeric_input)
+print('You said',numeric_input,', The Square Root of that is',sqrt_output)
